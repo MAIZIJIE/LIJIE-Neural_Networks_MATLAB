95961b3128036a7da10174d054cb1ebe2f603795
maizijie <49906885+MAIZIJIE@users.noreply.github.com>
2020-04-04 23:30:58 +0800

    Add files via upload

---
+++ VSM_2/vsm_test.m
@@ -0,0 +1,3 @@
+load heart_scale
+model = libsvmtrain(heart_scale_label, heart_scale_inst, '-c 1 -g 0.07');
+[predict_label, accuracy, dec_values] = libsvmpredict(heart_scale_label, heart_scale_inst, model);
